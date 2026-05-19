ee9988b85d1295933cf39bc585d7d61e36de3491
Mario Medrano <mario.medrano.maldonado@gmail.com>
2026-05-19 00:31:14 +0000

    Disable Neovim system clipboard prompts

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable automatic terminal/system clipboard access. In the Docker workstation on
+-- macOS, LazyVim defaults to unnamedplus, which can trigger clipboard permission
+-- prompts from the terminal while editing.
+vim.opt.clipboard = ""
