8dda7277aa496b1b4f6031e401f6db20fabdab2f
Mario Medrano <mario.medrano.maldonado@gmail.com>
2026-05-21 02:20:14 +0000

    More custimizations

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Set spacebar as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,13 @@
 
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+
+-- Exit Insert Mode instantly by smashing 'jk' on the home row
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
+
+-- Keep cursor dead-center on the screen when scrolling half-pages
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+
+-- Clear distracting search highlights easily with Space + h
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
